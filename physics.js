ea9deb301e1d354b10537b1729c30a40af8f5626
Evan <evanmckinney03@gmail.com>
2024-08-20 00:06:57 -0400

    Ball loses all acceleration and velocity after hitting floor

--- physics.js
+++ physics.js
@@ -1,21 +1,30 @@
 const TIME_INTERVAL = 5;
+const G = 60;
 
 window.onload = init;
 
 function init() {
   //velocities are in pixels per second
-  const circle = {svg:null, x:16, y:16, r:16, v_x:0, v_y: 0, a_x:20, a_y:20};
+  const circle = {svg:null, x:16, y:16, r:16, v_x:0, v_y: 0, a_x:0, a_y:G};
   createCircle(circle);
-  let s = setInterval(simulate, TIME_INTERVAL, circle);
+  let svg = document.getElementById('svg');
+  let bounds = [[0, svg.getAttribute('width')],[0, svg.getAttribute('height')]];
+  let s = setInterval(simulate, TIME_INTERVAL, circle, bounds);
 }
 
-function simulate(circle) {
+function simulate(circle, bounds) {
   //update velocity based on acceleration
   circle.v_x += (circle.a_x * (TIME_INTERVAL / 1000));
   circle.v_y += (circle.a_y * (TIME_INTERVAL / 1000));
   //update position based on velocity
   circle.x += (circle.v_x * (TIME_INTERVAL / 1000));
   circle.y += (circle.v_y * (TIME_INTERVAL / 1000));
+  //check if it hits the floor
+  //for now, make it completely stop once it hits the floor
+  if(circle.y > bounds[1][1] - circle.r) {
+    circle.v_y = 0;
+    circle.a_y = 0;
+  }
   circle.svg.setAttribute('cx', circle.x);
   circle.svg.setAttribute('cy', circle.y);
 }
